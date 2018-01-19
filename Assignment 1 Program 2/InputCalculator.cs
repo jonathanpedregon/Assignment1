e3ddec9b916de55246307fa21eeaa46d9e9ecfc5
Jonathan <jonathanpedregon@gmail.com>
2018-01-18 19:29:45 -0700

    Part 2 is now working.

---
+++ Assignment 1 Program 2/InputCalculator.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace Assignment_1_Program_2
+{
+    public class InputCalculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public int Sum { get; set; }
+        public int Difference { get; set; }
+        public int Product { get; set; }
+        public int Quotient{ get; set; }
+        public int Modulus { get; set; }
+        public string LessThanString { get; set; }
+        public string GreaterThanString { get; set; }
+        public string EqualString { get; set; }
+
+
+        public InputCalculator(int firstNumber, int secondNumber)
+        {
+            FirstNumber = firstNumber;
+            SecondNumber = secondNumber;
+            Sum = FirstNumber + SecondNumber;
+            Difference = FirstNumber - SecondNumber;
+            Product = FirstNumber * SecondNumber;
+            Modulus = FirstNumber % SecondNumber;
+            SetComparisonStrings();
+        }
+
+        public void SetComparisonStrings()
+        {
+            LessThanString = FirstNumber < SecondNumber ? "is less than" : "is not less than";
+            GreaterThanString = FirstNumber > SecondNumber ? "is greater than" : "is not greater than";
+            EqualString = FirstNumber == SecondNumber ? "equals" : "does not equal";
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{FirstNumber} + {SecondNumber} = {Sum}");
+            sb.AppendLine($"{FirstNumber} - {SecondNumber} = {Difference}");
+            sb.AppendLine($"{FirstNumber} * {SecondNumber} = {Product}");
+            sb.AppendLine($"{FirstNumber} / {SecondNumber} = {Quotient}");
+            sb.AppendLine($"{FirstNumber} % {SecondNumber} = {Modulus}");
+            sb.AppendLine();
+            sb.AppendLine($"{FirstNumber} {LessThanString} {SecondNumber}");
+            sb.AppendLine($"{FirstNumber} {GreaterThanString} {SecondNumber}");
+            sb.AppendLine($"{FirstNumber} {EqualString} {SecondNumber}");
+
+            return sb.ToString();
+        }
+    }
+}
--- Assignment 1 Program 2/Program.cs
+++ Assignment 1 Program 2/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Assignment_1_Program_2
 {
@@ -10,6 +7,34 @@ namespace Assignment_1_Program_2
     {
         static void Main(string[] args)
         {
+            AskForResponses();
+        }
+
+        static void AskForResponses()
+        {
+            int firstNumber, secondNumber;
+            bool validNumber;
+            do
+            {
+                Console.WriteLine("Please enter your first number: ");
+                var response = Console.ReadLine();
+                validNumber = TryToParse(response, out firstNumber);
+            } while (!validNumber);
+            do
+            {
+                Console.WriteLine("Please enter your second number: ");
+                var response = Console.ReadLine();
+                validNumber = TryToParse(response, out secondNumber);
+            } while (!validNumber);
+
+            var calculator = new InputCalculator(firstNumber, secondNumber);
+            Console.WriteLine(calculator.ToString());
+            Console.Read();
+        }
+
+        static bool TryToParse(string response, out int number)
+        {
+            return (int.TryParse(response, out number));
         }
     }
 }
