800fd3aaa51fdb6548bb36b25814f2a396681c83
Jonathan <jonathanpedregon@gmail.com>
2018-01-18 18:53:19 -0700

    Program 1 is working

--- Assignment1/Form1.cs
+++ Assignment1/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Assignment1
@@ -15,6 +8,23 @@ namespace Assignment1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e){}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text, button1.Text, MessageBoxButtons.AbortRetryIgnore,MessageBoxIcon.Exclamation);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox2.Text, button2.Text, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox3.Text, button3.Text, MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
         }
     }
 }
